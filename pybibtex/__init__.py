eb253af6c4c06c3e29139835f67d7c1ec3ca90f5
rasbt <mail@sebastianraschka.com>
2020-01-02 21:26:50 -0600

    pep8

--- pybibtex/__init__.py
+++ pybibtex/__init__.py
@@ -10,7 +10,7 @@ from .pybibtex import parse_bibfile_to_dict, parse_bibfile_to_cite_dict
 from .pybibtex import bibentry_to_style
 from .pybibtex import get_cites, build_citekey_dict, replace_citekeys
 
-__all__ = [parse_bibfile_to_dict, parse_bibfile_to_cite_dict, 
+__all__ = [parse_bibfile_to_dict, parse_bibfile_to_cite_dict,
            bibentry_to_style, get_cites, build_citekey_dict, replace_citekeys]
 
 __version__ = '0.3.0'
